79be180865640cb88f55c9340c537abc29a26cb1
Gustavo <gunaldoni@gmail.com>
2023-09-14 09:34:01 -0300

    Arquivos iniciais trabalho 02

---
+++ trab02/tokens.h
@@ -0,0 +1,16 @@
+#define IF 257
+#define ELSE 258
+#define WHILE 259
+#define DO 260
+#define INT 262
+#define GE 264
+#define LE 265
+#define NE 266
+#define EQ 267
+#define NUM 268
+#define ID 269
+
+
+
+
+
