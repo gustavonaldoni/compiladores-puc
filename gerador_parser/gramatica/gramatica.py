04bdefcbad08e8d2ed5dfe297d3d771bb68b6e60
Gustavo <gunaldoni@gmail.com>
2023-09-21 08:33:11 -0300

    Melhoria no código da função nullable

--- gerador_parser/gramatica/gramatica.py
+++ gerador_parser/gramatica/gramatica.py
@@ -1,14 +1,17 @@
-from gramatica.producao import Producao
+import re
 
-STRING_VAZIA = ''
+STRING_VAZIA = 'ε'
+
 
 class Gramatica:
-    def __init__(self, nao_terminais: set, terminais: set, producoes: list[Producao], simbolo_inicial: str) -> None:
+    def __init__(self, nao_terminais: set, terminais: set, producoes: dict, simbolo_inicial: str) -> None:
         self.nao_terminais = nao_terminais
         self.terminais = terminais
         self.producoes = producoes
         self.simbolo_inicial = simbolo_inicial
-    
+
+        self._log_nullable = './log_nullable.txt'
+
     def mostrar_nao_terminais(self):
         print(f'N = {self.nao_terminais}')
 
@@ -16,8 +19,7 @@ class Gramatica:
         print(f'T = {self.terminais}')
 
     def mostrar_producoes(self):
-        for producao in self.producoes:
-            producao.mostrar()
+        print(self.producoes)
 
     def mostrar_simbolo_inicial(self):
         print(f'S = {self.simbolo_inicial}')
@@ -30,3 +32,53 @@ class Gramatica:
         self.mostrar_simbolo_inicial()
         print()
 
+    def _nullable_escrever(self, simbolo: str, arquivo):
+        arquivo.write(f'{simbolo}: ')
+
+        if simbolo in self.terminais:
+            arquivo.write('0 ')
+            return False
+
+        if STRING_VAZIA in self.producoes[simbolo]:
+            arquivo.write('1 ')
+            return True
+
+        for derivacao in self.producoes[simbolo]:
+            arquivo.write('\n')
+            for s in derivacao:
+                self._nullable_escrever(s, arquivo)
+
+            arquivo.write('\n')
+
+    def nullable(self, simbolo: str) -> bool:
+        with open(self._log_nullable, mode='a+') as arquivo:
+            self._nullable_escrever(simbolo, arquivo)
+
+        arquivo = open(self._log_nullable, mode='r')
+
+        conteudo = arquivo.read()
+        conteudo = re.sub(r'[\n]{2,}', '\n\n', conteudo)
+
+        indice_primeira_quebra = conteudo.find('\n')
+        conteudo = conteudo[indice_primeira_quebra + 1:]
+
+        arquivo.close()
+
+        with open(self._log_nullable, mode='w') as arquivo:
+            arquivo.write(conteudo)
+
+        partes = conteudo.split('\n\n')
+        print(partes)
+        
+        for i, parte in enumerate(partes):
+            if '0' in parte:
+                partes[i] = False
+            else:
+                partes[i] = True
+        
+        print(partes)
+
+        if True in partes:
+            return True
+        
+        return False
--- gerador_parser/main.py
+++ gerador_parser/main.py
@@ -1,15 +1,17 @@
 from gramatica.gramatica import Gramatica
 from gramatica.gramatica import STRING_VAZIA
 
-from gramatica.producao import Producao
-
 if __name__ == '__main__':
+    producoes = {'S': ['AB'],
+                 'A': ['BC', 'aB', 'C'],
+                 'B': ['Cb'],
+                 'C': [STRING_VAZIA, 'c']}
+
     gramatica = Gramatica(nao_terminais=set({'A', 'B', 'C', 'S'}),
                           terminais=set({'a', 'b', 'c'}),
-                          producoes=list((Producao('S', list('AB')),
-                                          Producao('A', list('a')),
-                                          Producao('B', list('bC')),
-                                          Producao('C', list('c')))),
+                          producoes=producoes,
                           simbolo_inicial='S')
 
     gramatica.mostrar()
+
+    print(gramatica.nullable('A'))
